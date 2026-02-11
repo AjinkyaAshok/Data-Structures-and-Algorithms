0b66294ad2a5994819c2a4d86a256f9fd7dd533b
ajinkyaashok <ajinkyaashokghate@gmail.com>
2026-02-11 21:22:38 +0530

    star pattern

--- dsa.js
+++ dsa.js
@@ -1,5 +1,18 @@
-function add(a,b){
-  let sum  =a+b
-  return sum
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n; j++) {
+    row = row + "*";
+  }
+  console.log(row);
 }
-console.log(add(2,44)) 
+
+let k = 5;
+for (let i = 0; i < k; i++) {
+  let row =''
+  for (let j = 0; j <= i; j++) {
+    row = row + (i+1);
+  }
+  console.log(row)
+}
