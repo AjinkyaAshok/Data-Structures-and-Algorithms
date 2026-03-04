c5be9ddd1a9def244704efc2ddc7b0740d55ff67
ajinkyaashok <ajinkyaashokghate@email.com>
2026-03-04 15:17:49 +0530

    485. Max Consecutive Ones

---
+++ 485. Max Consecutive Ones.js
@@ -0,0 +1,15 @@
+var findMaxConsecutiveOnes = function (nums) {
+    let currentCount = 0;
+    let maxCount = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            currentCount++
+        } else {
+            currentCount = 0;
+        }
+        if (maxCount < currentCount) {
+            maxCount = currentCount
+        }
+    }
+    return maxCount;
+};
