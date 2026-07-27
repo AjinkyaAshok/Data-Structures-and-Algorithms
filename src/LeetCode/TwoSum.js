5ba420379812700ddaf285d67b36d8ba28a45289
ajinkyaashok <ajinkyaashokghate@gmail.com>
2026-07-27 14:02:38 +0530

    leetcode:two sum

---
+++ src/LeetCode/TwoSum.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+
+        const need = target - nums[i];
+
+        if (map.has(need)) {
+            return [map.get(need), i];
+        }
+
+        map.set(nums[i], i);
+    }
+}
+
+console.log(twoSum([2,7,11,15],9));
