84dcb1edd0be37b947fff6f978542bc218e3cc6f
ajinkyaashok <ajinkyaashokghate@gmail.com>
2026-08-31 19:47:49 +0530

    leetcode: add largest odd number finder from string

---
+++ src/LeetCode/125. Valid Palindrome.js
@@ -0,0 +1,15 @@
+//approach 1
+function reverseStr(s) {
+  s = s.toLowerCase();
+  let filterStr = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].match(/[a-z0-9]/i)) {
+      filterStr += s[i];
+    }
+  }
+  let rev = filterStr.split("").reverse().join("");
+  return filterStr === rev;
+}
+
+console.log(reverseStr("A man, a plan, a canal: Panama"));
+
